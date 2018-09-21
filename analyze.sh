874632a8c2ae426b634b8526ab835c091344e669
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-21 04:47:11 +0000

    add avatar url to discord announcer

--- analyze.sh
+++ analyze.sh
@@ -2,5 +2,8 @@
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 python "${thisdir}/analyze.py" > "${thisdir}/logs/analyze.log"
-bash "${thisdir}/test-deps/discord.sh" --username "Stars GDT Analyzer" --text "$(cat "${thisdir}/logs/analyze.log")"
+bash "${thisdir}/test-deps/discord.sh" \
+    --username "Stars GDT Analyzer" \
+    --avatar-url "https://raw.githubusercontent.com/ChaoticWeg/stars-gdt/master/assets/analyze.png" \
+    --text "$(cat "${thisdir}/logs/analyze.log")"
 
