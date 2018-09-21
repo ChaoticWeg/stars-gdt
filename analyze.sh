fb74b877441804ac9616487edc09a00f02f6d388
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-21 04:59:32 +0000

    convert analyze announcement to embed

--- analyze.sh
+++ analyze.sh
@@ -5,5 +5,8 @@ python "${thisdir}/analyze.py" > "${thisdir}/logs/analyze.log"
 bash "${thisdir}/test-deps/discord.sh" \
     --username "Stars GDT Analyzer" \
     --avatar-url "https://raw.githubusercontent.com/ChaoticWeg/stars-gdt/master/assets/analyze.png" \
-    --text "$(cat "${thisdir}/logs/analyze.log")"
+    --title "Stars GDT Analysis" \
+    --color "0x006847" \
+    --timestamp \
+    --description "$(cat "${thisdir}/logs/analyze.log")"
 
