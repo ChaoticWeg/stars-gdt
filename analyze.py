e7beecc10b8668d4e945b46f61e076ee663bffc5
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-22 04:38:10 +0000

    dump next game time to log file

--- analyze.py
+++ analyze.py
@@ -12,22 +12,26 @@ flatten_dates = lambda dates: [ game for date in dates for game in date["games"]
 def run():
     """ main executing function """
 
+    # API wants UTC
     start_date = datetime.utcnow() - timedelta(days=1)
     end_date   = datetime.utcnow() + timedelta(days=4)
 
+    # fetch using start and end dates
     sched = nhl.fetch_schedule(start_date, end_date)
 
+    # if we don't have any games in the range, bail out
     if sched["totalGames"] < 1:
         print("looks like there aren't any games for a while! bailing out.")
         return
     
+    # filter and flatten
     all_dates      = sched["dates"]
     relevant_dates = filter_dates(all_dates)
     upcoming_games = flatten_dates(relevant_dates)
 
     # ZULU TIME
     next_game_date = min([ dt_parser.parse(game["gameDate"]) for game in upcoming_games ])
-    with open("logs/next_game_date.txt", "w") as outfile:
+    with open("logs/next_game_date.log", "w") as outfile:
         outfile.write(str(next_game_date))
 
 if __name__ == "__main__":
--- analyze.sh
+++ analyze.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+export TZ='America/Chicago'
+parse_date() { date -d "$(cat "${thisdir}/logs/next_game_date.log")" +"%A, %B %Y at %H:%M %Z"; };
+
 python "${thisdir}/analyze.py" > "${thisdir}/logs/analyze.log"
 bash "${thisdir}/test-deps/discord.sh" \
     --username "Stars GDT Analyzer" \
     --avatar-url "https://raw.githubusercontent.com/ChaoticWeg/stars-gdt/master/assets/avatars/analyze.png" \
-    --title "Stars GDT Analysis" \
+    --title "Next Game Time" \
     --color "0x006847" \
     --timestamp \
-    --description "$(cat "${thisdir}/logs/analyze.log")"
+    --description "The Stars' next game is $(parse_date)" \
+    --text "$(cat "${thisdir}/logs/analyze.log")"
 
