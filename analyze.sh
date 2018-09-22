94b633feb0b6f742de33954cf81d5697c8209ca8
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-22 03:45:56 +0000

    update avatar url

--- analyze.sh
+++ analyze.sh
@@ -4,7 +4,7 @@ thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 python "${thisdir}/analyze.py" > "${thisdir}/logs/analyze.log"
 bash "${thisdir}/test-deps/discord.sh" \
     --username "Stars GDT Analyzer" \
-    --avatar-url "https://raw.githubusercontent.com/ChaoticWeg/stars-gdt/master/assets/analyze.png" \
+    --avatar-url "https://raw.githubusercontent.com/ChaoticWeg/stars-gdt/master/assets/avatars/analyze.png" \
     --title "Stars GDT Analysis" \
     --color "0x006847" \
     --timestamp \
