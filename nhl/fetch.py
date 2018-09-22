e47608259081e634354c94276340fe8c271ae74c
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-21 22:39:23 -0500

    make start date and end date non-optional params

--- nhl/fetch.py
+++ nhl/fetch.py
@@ -1,12 +1,13 @@
 from .urls import url_schedule
+from datetime import datetime
 import requests
 
 STARS_ID = 25
 
-def fetch_schedule(team_id = STARS_ID):
+def fetch_schedule(start_date, end_date, team_id = STARS_ID):
     """ fetch the team's schedule for the day. team_id optional param, default to stars """
 
-    url = url_schedule(team_id)
+    url = url_schedule(team_id, start_date, end_date)
 
     # fetch, validate, and parse json
     req = requests.get(url)
--- nhl/urls.py
+++ nhl/urls.py
@@ -3,18 +3,15 @@ from datetime import datetime
 DT_FORMAT = "%Y-%m-%d"                      # datetime format
 BASE_URL  = "https://statsapi.web.nhl.com"  # base API url
 
-def url_schedule(team_id, season = "20182019"):
+def url_schedule(team_id, start_date, end_date):
     """ Get the fully qualified API URL from which to grab team schedule """
-
-    # TODO FIXME add search date
-    search_date = datetime.utcnow().strftime(DT_FORMAT)
 
     # dict: URL querystring params
     params = {
         "site": "en_nhl",
-        "season": season,
-        "startDate": search_date,
-        "endDate": search_date,
+        "season": "20182019",
+        "startDate": start_date.strftime(DT_FORMAT),
+        "endDate": end_date.strftime(DT_FORMAT),
         "teamId": team_id,
         "hydrate": "team(leaders(categories=[points,goals,assists],gameTypes=[P])),linescore,broadcasts(all),tickets,game(content(media(epg),highlights(scoreboard)),seriesSummary),radioBroadcasts,metadata,decisions,scoringplays,seriesSummary(series)" # TODO oh god the horror
     }
